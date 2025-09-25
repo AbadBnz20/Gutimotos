845cf775904241b466dcbb19e8202443ce060269
AbadBnz25 <benizsossaabad2@gmail.com>
2025-09-25 15:45:43 -0400

    cambio loading2

--- src/auth/providers/CheckAuthProvider.tsx
+++ src/auth/providers/CheckAuthProvider.tsx
@@ -2,11 +2,10 @@ import type { PropsWithChildren } from "react";
 import { useAuthStore } from "../store/auth.store";
 import { useQuery } from "@tanstack/react-query";
 import { CustomFullScreenLoading } from "../components/CustomFullScreenLoading";
-import { Navigate } from "react-router";
 
 export const CheckAuthProvider = ({ children }: PropsWithChildren) => {
   const { checkAuthStatus } = useAuthStore();
-  const { isLoading, isError } = useQuery({
+  const { isLoading,  } = useQuery({
     queryKey: ["auth"],
     queryFn: checkAuthStatus,
     retry: false,
@@ -14,7 +13,7 @@ export const CheckAuthProvider = ({ children }: PropsWithChildren) => {
     refetchOnWindowFocus: false,
   });
   if (isLoading) return <CustomFullScreenLoading />;
-  if (isError) return <Navigate to="/auth/login" />;
+  // if (isError) return <Navigate to="/auth/login" />;
 
   return children;
 };
